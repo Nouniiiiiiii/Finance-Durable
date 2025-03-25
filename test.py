c4875bdaeb9da9026fe7a8fd16006041e5d04f9f
karimhajjidau <162303355+karimhajjidau@users.noreply.github.com>
2025-03-25 12:55:12 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title("Hello Streamlit 👋")
 st.write("Si tu vois ça, Streamlit fonctionne parfaitement !")
+
+
+import yfinance as yf
+
+ticker = yf.Ticker("AAPL")  # Apple Inc.
+
+# Données ESG
+esg = ticker.sustainability
+print(esg)
